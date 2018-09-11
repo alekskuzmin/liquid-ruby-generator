53de5f3060518be090b84322b696b16859fff7c3
Alex Kuzmin <alex.kuzmin@protonmail.ch>
2018-09-12 00:29:08 +0200

    Added the camelcase and uncapitalize filters. Updated the readme.

--- ModuleGenerator.rb
+++ ModuleGenerator.rb
@@ -5,28 +5,46 @@ require 'fileutils'
 moduleName = ARGV[0]
 templatesFolder = "./ModuleTemplate"
 
-print "You're about to generate a new module called " + moduleName + ". Searching the templates in " + templatesFolder + "...\n"
+print "You're about to generate a new module called '" + moduleName + "'. Searching the templates in " + templatesFolder + "...\n"
 
-Dir[templatesFolder + '/**/*.liquid'].each do |fname|
-    @template = Liquid::Template.parse(File.open(fname, "r").read)
-    print "***********************************************************\n"
-    print "Found " + fname + "\n"
-    renderedTemplate = @template.render('module_name' => moduleName)
-    print "Rendered template for " + fname + " is:\n"
-    print "***********************************************************\n"
-    print renderedTemplate
-    print "***********************************************************\n"
-    extn = File.extname(fname)
-    file_name = File.basename(fname, extn)
-    path = Pathname.new(File.dirname(fname))
-    root  = Pathname.new(templatesFolder)
-    relativePath = path.relative_path_from(root)
-    dir_name = moduleName.downcase + "/" + relativePath.to_s + "/"
-    response = FileUtils.mkdir_p(dir_name)
-    generatedFileName = "./" + dir_name + moduleName + file_name
-    print "Writing it to " + generatedFileName + "...\n"
-    File.open(generatedFileName, "w") {|f| f.write(renderedTemplate) }
+module TextFilter
+    def camelcase(input) 
+        input.split('-').map(&:capitalize).join
+    end
+    def uncapitalize(input)
+        input[0, 1].downcase + input[1..-1]
+    end
 end
 
-print "***********************************************************\n"
-print "Your new module is ready.\n"
+Liquid::Template.register_filter(TextFilter)
+
+templates = templatesFolder + '/**/*.liquid'
+if File.directory?(templatesFolder)    
+    Dir[templates].each do |fname|
+        @template = Liquid::Template.parse(File.open(fname, "r").read)
+        print "***********************************************************\n"
+        print "Found " + fname + "\n"
+        renderedTemplate = @template.render('module_name' => moduleName)
+        print "Rendered template for " + fname + " is:\n"
+        print "***********************************************************\n"
+        print renderedTemplate
+        print "***********************************************************\n"
+        extn = File.extname(fname)
+        file_name = File.basename(fname, extn)
+        path = Pathname.new(File.dirname(fname))
+        root  = Pathname.new(templatesFolder)
+        relativePath = path.relative_path_from(root)
+        dir_name = moduleName.downcase + "/" + relativePath.to_s + "/"
+        response = FileUtils.mkdir_p(dir_name)
+        @filenameTemplate = Liquid::Template.parse(file_name)
+        camelCaseFileName = @filenameTemplate.render('module_name' => moduleName)
+        camelCaseFileName = camelCaseFileName.split('-').map(&:capitalize).join
+        generatedFileName = "./" + dir_name + camelCaseFileName        
+        print "Writing it to " + generatedFileName + "...\n"
+        File.open(generatedFileName, "w") {|f| f.write(renderedTemplate) }
+    end
+    print "***********************************************************\n"
+    print "Your new module is ready.\n"
+else
+    print "Templates not found!\n"
+end
